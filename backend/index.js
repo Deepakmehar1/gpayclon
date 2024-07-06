2a6a97db335c5860928a0bb510113b2f2c178ac3
Deepak Mehar <deepakmehar397@gmail.com>
2024-07-06 14:20:12 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -18,4 +18,18 @@ app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/user"));
 
+// comment out
+const path = require("path");
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is running");
+  });
+}
+
 app.listen(PORT, () => console.log("server is running on", PORT));
